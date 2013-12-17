c1b90be3ad426cdbca2c28aa5f3dab9ebf61c15f
notcome <notcome@me.com>
2013-12-17 17:02:30 +0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+#Redis-pppd
+
+Redis-pppd is a pretty simple pppd plugin using Redis as authentication database. It was designed to replace my vulnerable file-based PPTP user info database. Rather than a full-functionality plugin, you'd better take it as a example of how to write plugins for pppd.
+
+##Compile & Install
+
+Before compiling, you need to modify the following line:
+
+	#define COMMAND "GET com.example.pppd."
+	
+It refers to the prefix of your pppd user information in Redis.
+
+To compile it, you need to have hiredis and pppd installed.
+
+	gcc -c -O redis-pppd.c -fPIC
+	gcc -shared -o redis-pppd.so redis-pppd.o -lhiredis
+	
+Move it to /usr/lib/pppd/<Current pppd version>/ :
+
+	mv redis-pppd.so /usr/lib/pppd/2.4.5/redis-pppd.so
+	
+Add the following line to your pppd config file:
+
+	plugin redis-pppd.so
+	
+Enjoy it!
+
+##Author & Licence
+Redis-pppd was written by Minsheng Liu (notcome at me dot com) and is released under the BSD license.
---
+++ redis-pppd.c
@@ -0,0 +1,75 @@
+#include <pppd/pppd.h>
+#include <pppd/chap-new.h>
+#include <hiredis/hiredis.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define COMMAND "GET com.example.pppd."
+
+char pppd_version[] = VERSION;
+
+redisContext *context = NULL;
+
+static int connectRedis (const char* ipAddress, int port) {
+    context = redisConnect(ipAddress, port);
+
+    if (context->err) {
+        error("Connection error: %s\n", context->errstr);
+        return 1;
+    }
+    return 0;
+}
+
+static void freeRedis (void *opaque, int arg) {
+    redisFree(context);
+    error("Redis context freed.\n");
+}
+
+static int pppdChapCheck (void) { return 1; }
+
+static int pppdChapVerify (char *user, char *ourname, int id,
+                           struct chap_digest_type *digest,
+                           unsigned char *challenge, unsigned char *response,
+                           char *message, int message_space) {
+    char command [160] = COMMAND
+    char *p = command;
+    while (*p) p ++;
+    char *q = user;
+    while (*q) {
+        *p = *q;
+        p ++; q ++;
+    }
+
+    redisReply *reply = redisCommand(context, command);
+
+    if (reply->type == REDIS_REPLY_NIL) {
+        error("User<%s> doesn't exist.\n", user);
+        return 0;
+    }
+    else if (reply->type != REDIS_REPLY_STRING) {
+        error("Bad value in redis key: %s.\n", command[4]);
+    }
+    else if (digest->verify_response(id, user, reply->str, strlen(reply->str),
+                                     challenge, response, message, message_space)) {
+        return 1;
+    }
+    else {
+        error("Wrong password.\n");
+        return 0;
+    }
+}
+
+static int checkAddressAllowed (unsigned int addr) { return 1; }
+
+void plugin_init(void) {
+    chap_check_hook = pppdChapCheck;
+    chap_verify_hook = pppdChapVerify;
+    allowed_address_hook = checkAddressAllowed;
+
+    if (connectRedis("127.0.0.1", 6379)) {
+        error("Failed to load plugin redis-pppd.\n");
+        exit(1);
+    }
+
+    add_notifier(&exitnotify, freeRedis, NULL);
+}
